8d02bbde790526f311169e5c3d66453d20346d3b
Yadav-Nilesh <nileshyadav9868@gmail.com>
2025-07-02 20:47:54 +0530

    Timer and Chat Fixed

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://devmeet-xp51.onrender.com"
+    : "http://localhost:5000";
+
+export const socket = io(URL, {
   autoConnect: false, // 🔒 Don't connect immediately
 });
